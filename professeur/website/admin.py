8c16e19bf6172a6504d4e51121d616eee285b82e
OBAMARIE13 <obaouattara@gmail.com>
2021-08-21 15:14:39 +0200

    christelle

--- professeur/website/admin.py
+++ professeur/website/admin.py
@@ -1,3 +1,55 @@
 from django.contrib import admin
+from . import models
+from django.utils.safestring import mark_safe
 
 # Register your models here.
+
+@admin.register(models.Banner)
+class BannerAdmin(admin.ModelAdmin):
+	list_display = ('view_photo', 'profession_un', 'profession_deux', 'nom', 'citation', 'date_add', 'date_update', 'status')
+	
+ 
+	def view_photo(self, obj):
+		return mark_safe(f'<img src="{obj.photo.url}" style="height:80px; width:130px">')
+	view_photo.short_description = 'Apercu des images view_photo'
+ 
+
+@admin.register(models.Gallerie)
+class GallerieAdmin(admin.ModelAdmin):
+	list_display = ('view_photo', 'nom', 'description', 'date_add', 'date_update', 'status')
+	
+ 
+	def view_photo(self, obj):
+		return mark_safe(f'<img src="{obj.photo.url}" style="height:80px; width:130px">')
+	view_photo.short_description = 'Apercu des images view_photo'
+
+
+@admin.register(models.About_detail)
+class About_detailAdmin(admin.ModelAdmin):
+	list_display = ('titre_profession', 'description', 'date_add', 'date_update', 'status')
+
+
+@admin.register(models.Liens_sociaux)
+class Liens_sociauxAdmin(admin.ModelAdmin):
+	list_display = ('icone', 'nom', 'lien', 'date_add', 'date_update', 'status')
+
+@admin.register(models.Contacts)
+class ContactsAdmin(admin.ModelAdmin):
+	list_display = ('nom', 'email', 'messages', 'date_add', 'date_update', 'status')
+
+@admin.register(models.Banner_skills)
+class Banner_skillsAdmin(admin.ModelAdmin): 
+    list_display = ('view_image', 'date_add', 'date_update', 'status')
+    
+    def view_image(self, obj):
+        return mark_safe(f'<img src="{obj.image.url}" style="height:80px; width:130px">')
+    view_image.short_description = 'Apercu des images view_photo'
+
+
+@admin.register(models.Banner_coursses)
+class Banner_courssesAdmin(admin.ModelAdmin): 
+    list_display = ('view_image', 'description_image', 'date_add', 'date_update', 'status')
+    
+    def view_image(self, obj):
+        return mark_safe(f'<img src="{obj.image.url}" style="height:80px; width:130px">')
+    view_image.short_description = 'Apercu des images view_photo'
--- professeur/website/models.py
+++ professeur/website/models.py
@@ -1,13 +1,15 @@
+from django.core.checks import messages
 from django.db import models
 
 # Create your models here.
 
 
 class Banner(models.Model):
-    image = models.FileField(upload_to="image_site")
-    profession = models.CharField(max_length=255)
+    photo = models.FileField(upload_to="image_website")
+    profession_un = models.CharField(max_length=255)
+    profession_deux = models.CharField(max_length=255)
     nom = models.CharField(max_length=200)
-    description = models.TextField()
+    citation = models.TextField()
     status = models.BooleanField(default=False)
     date_add = models.DateTimeField(auto_now=True)
     date_update = models.DateTimeField(auto_now=True)
@@ -17,16 +19,17 @@ class Banner(models.Model):
         return self.nom
     
     
-class About(models.Model):
-    titre = models.CharField(max_length=200)
-    description = models.TextField()        
+class Gallerie(models.Model):
+    photo = models.FileField(upload_to="image_website")
+    nom = models.CharField(max_length=255)
+    description = models.CharField(max_length=200)       
     status = models.BooleanField(default=False)
     date_add = models.DateTimeField(auto_now=True)
     date_update = models.DateTimeField(auto_now=True)
     
     
-    def __str__(self) -> str:
-        return self.titre
+    def __str__(self):
+        return self.nom
     
     
 class About_detail(models.Model):
@@ -51,3 +54,35 @@ class Liens_sociaux(models.Model):
     
     def __str__(self):
         return self.nom
+    
+
+class Contacts(models.Model):
+    nom = models.TextField
+    email = models.CharField(max_length=200)
+    messages = models.TextField()
+    status = models.BooleanField(default=False)
+    date_add = models.DateTimeField(auto_now=True)
+    date_update = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.nom
+
+
+class Banner_skills(models.Model):
+    image = models.FileField(upload_to="image_website")
+    status = models.BooleanField(default=False)
+    date_add = models.DateTimeField(auto_now=True)
+    date_update = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.image 
+    
+class Banner_coursses(models.Model):
+    image = models.FileField(upload_to="image_website")
+    description_image = models.CharField(max_length=255)
+    status = models.BooleanField(default=False)
+    date_add = models.DateTimeField(auto_now=True)
+    date_update = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.description_image 
